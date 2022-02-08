55bbde87b82e792ce94e63fab1cd1357e2c3b9b0
Andrei Pervyi <taxidevelop@yandex.ru>
2022-02-08 21:09:17 +0400

    Admin-panel

--- travels/country/admin.py
+++ travels/country/admin.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from country.models import *
 
 
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'population', 'time_create', 'photo', 'is_published')
-    list_display_links = ('id', 'title')
+    list_display = ('id', 'title', 'population', 'time_create', 'get_html_photo', 'is_published')
+    list_display_links = ('id', 'title', 'get_html_photo')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {"slug": ("title",)}
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'get_html_photo', 'is_published', 'time_create', 'time_update')  # В поле редактирования
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')  # Только для чтения
+    # save_on_top = True  # Дублирует строку о сохраниении вверху
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f'<img src="{ object.photo.url }" width=70>')
+
+    get_html_photo.short_description = "Миниатрюра"
 
 
 class ContinentAdmin(admin.ModelAdmin):
@@ -21,3 +31,6 @@ class ContinentAdmin(admin.ModelAdmin):
 
 admin.site.register(Country, CountryAdmin)
 admin.site.register(Continent, ContinentAdmin)
+
+admin.site.site_title = 'Админ-панель сайта о путешествиях'
+admin.site.site_header = 'Админ-панель сайта о путешествиях'
